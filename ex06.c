f8a4a5abc8f8502bb552266456ee15ebef51af70
UĞUR <yalinugur00@outlook.com>
2024-11-02 16:40:53 +0300

    Update ex06.c

--- ex06.c
+++ ex06.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main() {
+    int numbers[17];
+    int index = 0;
+
+    for (int i = 99; i >= 51; i -= 3) {
+        numbers[index] = i;
+        index++;
+    }
+
+    printf("Numbers divisible by 3 from 100 to 50:\n");
+    for (int i = 0; i < index; i++) {
+        printf(" %d ", numbers[i]);
+        if (i < index - 1) {
+            printf(" \n ");
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
