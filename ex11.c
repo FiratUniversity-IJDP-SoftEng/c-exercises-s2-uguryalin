e0d1e4e13c7d8fa0127d211b8620f200150d7f23
UĞUR <yalinugur00@outlook.com>
2024-11-02 17:59:38 +0300

    Update ex11.c

--- ex11.c
+++ ex11.c
@@ -1,1 +1,76 @@
+int main() {
+    int size = 5;
+    
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (j == i || j == (size - 1 - i)) {
+                printf("*");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
 
+//output
+
+/*
+*    * 
+ *  * 
+  ** 
+ *  * 
+*    * 
+*/
+
+
+//My own code 
+
+#include <stdio.h>
+
+int main() {
+    int size = 6; 
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (j == i || j == (size - 1 - i)) {
+                printf("*");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("  ");
+
+        for (int j = 0; j < size; j++) {
+            if ((j == 0 || j == size - 1) && (i > 0 && i < size - 1)) {
+                printf("O");
+            } else if ((i == 0 || i == size - 1) && (j > 0 && j < size - 1)) {
+                printf("O");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("  ");
+        
+        for (int j = 0; j < size; j++) {
+            if (j == i || j == (size - 1 - i)) {
+                printf("*");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+// output of my code
+/*
+*    *   OOOO   *    *
+ *  *   O    O   *  * 
+  **    O    O    **  
+  **    O    O    **  
+ *  *   O    O   *  * 
+*    *   OOOO   *    *
+*/
